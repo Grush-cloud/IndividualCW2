7047dfbf512a791a2f86a950805d1581b5372f82
Grush-cloud <georgeashinze@gmail.com>
2021-06-05 14:59:53 +0400

    1

--- server.js
+++ server.js
@@ -1,46 +1,40 @@
-//import modules
-const exp = require('express')
-const { ObjectID } = require('mongodb')
-var path = require("path");
+// Import dependencies modules:
+const express = require('express')
+// const bodyParser = require('body-parser')
 
-//create an express js instance
-const app = exp()
 
-//config express js
-app.use(exp.json())
-const port = process.env.PORT || 3000
-app.use((req, res, next) => {
-    res.header("Access-Control-Allow-Headers", "*");
+// Create an Express.js instance:
+const app = express()
+
+// config Express.js
+app.use(express.json())
+app.set('port', 3000)
+app.use ((req,res,next) => {
     res.setHeader('Access-Control-Allow-Origin', '*');
-
-    next()
+    next();
 })
 
-//Connect to mongodb
-const MongoClient = require('mongodb').MongoClient
-let db
-MongoClient.connect('mongodb+srv://George:<password>@cluster0.dtiun.mongodb.net', (err, client) => {
+// connect to MongoDB
+const MongoClient = require('mongodb').MongoClient;
+let db;
+MongoClient.connect('mongodb+srv://George:Nelsonattwo@cluster0.dtiun.mongodb.net', (err, client) => {
     db = client.db('Webstore')
 })
 
-//middleware to handle images 
-app.get('/collection/lessons/img', (req, res, next) => {
-    var imagePath = path.resolve(__dirname, "img"); 
-    app.use("/img", express.static(imagePath));
-})
 
-//display a message or root path to show that API is working
+// dispaly a message for root path to show that API is working
 app.get('/', (req, res, next) => {
-    res.send('Select a collection, e.g., collection/messages')
+    res.send('Select a collection, e.g., /collection/messages')
 })
 
-//get collection
+// get the collection name
 app.param('collectionName', (req, res, next, collectionName) => {
     req.collection = db.collection(collectionName)
+    // console.log('collection name:', req.collection)
     return next()
 })
 
-//retrieve all the objects from collection
+// retrieve all the objects from an collection
 app.get('/collection/:collectionName', (req, res, next) => {
     req.collection.find({}).toArray((e, results) => {
         if (e) return next(e)
@@ -48,40 +42,51 @@ app.get('/collection/:collectionName', (req, res, next) => {
     })
 })
 
-//posting new data to the collection
+//adding post
 app.post('/collection/:collectionName', (req, res, next) => {
-    req.collection.insert(req.body, (e, results) => { 
-        if (e) return next(e)    
-        res.send(results.ops) 
-    })
+req.collection.insert(req.body, (e, results) => {
+if (e) return next(e)
+res.send(results.ops)
+})
 })
 
-//update object
+// return with object id 
+const ObjectID = require('mongodb').ObjectID;
+app.get('/collection/:collectionName/:id'
+, (req, res, next) => {
+req.collection.findOne({ _id: new ObjectID(req.params.id) }, (e, result) => {
+if (e) return next(e)
+res.send(result)
+})
+})
+
+
+//update an object 
+
 app.put('/collection/:collectionName/:id', (req, res, next) => {
-    req.collection.update(
-        { _id: new ObjectID(req.params.id) },
-        { $set: req.body },
-        { safe: true, multi: false },
-        (e, result) => {
-            if (e) return next(e)
-            res.send((result.result.n === 1) ? { msg: 'success' } : { msg: 'error' })
-        }
-    )
+req.collection.update(
+{_id: new ObjectID(req.params.id)},
+{$set: req.body},
+{safe: true, multi: false},
+(e, result) => {
+if (e) return next(e)
+res.send((result.result.n === 1) ? {msg: 'success'} : {msg: 'error'})
+})
 })
 
 
-//delete object
+
+
+
 app.delete('/collection/:collectionName/:id', (req, res, next) => {
-    req.collection.deleteOne(
-        { _id: ObjectID(req.params.id) },
-        (e, result) => {
-            if (e) return next(e)
-            res.send((result.result.n === 1) ? { msg: 'success' } : { msg: 'error' })
-        }
-    )
+req.collection.deleteOne(
+{ _id: ObjectID(req.params.id) },(e, result) => {
+if (e) return next(e)
+res.send((result.result.n === 1) ?
+{msg: 'success'} : {msg: 'error'})
+})
 })
 
-//start server
-app.listen(port, () => {
-    console.log('Express js server runnning on localhost:3000')
-})
+
+const port = process.env.PORT || 3000
+app.listen(port)
